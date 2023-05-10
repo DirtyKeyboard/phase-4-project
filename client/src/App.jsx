1a3b5be7e4b5768093f694280a14aeae07f30eb9
joshuathawke <joshuathawke@gmail.com>
2023-05-10 15:40:33 -0400

    homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import Login from './Login';
 import Create from './Create'
+import Dashboard from './Dashboard';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path='/login' element={<Login />} />
       <Route path='/create_account' element={<Create />} />
+      <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   )
 }
