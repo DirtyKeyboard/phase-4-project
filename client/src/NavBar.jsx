8cb49de0d5a8a7b3bc98e4f58dde1c75759d7475
DirtyKeyboard <andrewxx000222@icloud.com>
2023-05-10 18:31:47 -0500

    fixed logout

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
 import userIcon from './assets/user.png'
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 const NavBar = () => {
+    const nav = useNavigate()
     const [show, setShow] = useState(false)
     return (
         <nav className=" w-[225px] bg-gray-900 h-screen fixed flex flex-col justify-start py-4 px-8 text-white">
@@ -12,7 +14,7 @@ const NavBar = () => {
                 className="w-12 h-12 rounded-full cursor-pointer float-left mb-8"
                 onClick={() => setShow(!show)}
             />
-            {show ? <button className="bg-red-700 p-2 rounded-full mb-8 hover:bg-red-600" onClick={() => {axios.get('api/logout'); window.location.reload()}}>Log Out?</button> : null}
+            {show ? <button className="bg-red-700 p-2 rounded-full mb-8 hover:bg-red-600" onClick={async() => {await axios.get('api/logout'); nav('/'); window.location.reload()}}>Log Out?</button> : null}
             <a href="/mybeats" className="text-xl font-semibold mb-4">
                 My Beats
             </a>
