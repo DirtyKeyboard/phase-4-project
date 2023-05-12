4001a59fbb1de5e55d71eee4ef81bb3508978184
DirtyKeyboard <andrewxx000222@icloud.com>
2023-05-12 14:07:00 -0500

    dev db

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = b'\x8bA\xa5\xe6bE\x83\x10xQ\x06\xcb'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:h8E+mc9i7RjhuUh@db.ndzxbeztcymzjjfyitku.supabase.co:5432/postgres" #sqlite:///songpicker.db
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///songpicker.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SESSION_COOKIE_SAMESITE"] = "None"
 app.config["SESSION_COOKIE_SECURE"] = True
